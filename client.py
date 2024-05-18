2f4c6e55698e8e74d19b27de609691a7962f8210
Daniel <jos.god.mel.10@gmail.com>
2024-05-17 18:58:27 -0600

    correct client

--- client.py
+++ client.py
@@ -1,25 +1,32 @@
 import socket
 import sys
 def main():
-    try: 
-        socket_cliente=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except socket.error as err:
-        print("No se pudo crear el socket")
-        sys.exit()
-    print("socket creado")
-    target_host="127.0.0.1"
-    target_port="9500"
-    BUFSIZE=256
-    payload="Hola servidor"
-    socket_cliente.connect((target_host,int(target_port)))
     try:
-        while true:
-            print("socket conectado a %s", target_host)
-            socket_cliente.send(payload.encode("UTF-8"))
-            print("Mensaje del servidor")
+        socket_cliente=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    except socket.error as err:
+        print("no se pudo crear el socket")
+        sys.exit()
+        print("socket creado")
+        target_host="172.16.0.127"
+        target_port="9500"
+        BUFSIZE=256
+        payload='hola servidor'
+        socket_cliente.connect((target_host,int(target_port)))
+    print("socket creado")
+    
+    try:
+        target_host="172.16.0.127"
+        target_port="9500"
+        BUFSIZE=256
+        payload='HOLAAAA'
+        socket_cliente.connect((target_host,int(target_port)))
+        while True:
+            print("socket conectado a ",target_host)
+            socket_cliente.send(payload.encode('utf-8'))
+            print("mensaje del servidor")
             data=socket_cliente.recv(BUFSIZE)
             print(data)
-    except keyboardInterrupt:
-        print("Condicion terminada por el usuario")
-        sys.exiit()
+    except KeyboardInterrupt:
+        print("Conexion terminada por el usuario")
+        sys.exit()
 main()
